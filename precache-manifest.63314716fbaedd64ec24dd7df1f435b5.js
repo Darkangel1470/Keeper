50d30060ffd8a9d4735b1e3e81738f5674aa7f84
nihar <navadkunder@gmail.com>
2022-05-09 19:57:27 +0530

    Updates

---
+++ precache-manifest.63314716fbaedd64ec24dd7df1f435b5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2daa16c2052e06cee048a40096c9e1df",
+    "url": "/Keeper/index.html"
+  },
+  {
+    "revision": "8b959ee2a6658c0d0299",
+    "url": "/Keeper/static/js/2.235e61d4.chunk.js"
+  },
+  {
+    "revision": "26263f466bc8a612a730",
+    "url": "/Keeper/static/js/main.021e609b.chunk.js"
+  },
+  {
+    "revision": "c2cd02bec1164cebc775",
+    "url": "/Keeper/static/js/runtime-main.8d08ef39.js"
+  }
+]);
